d9640baa6c9de7d65fc30c8c9c9edea7ae0a7b43
codepreneur <contramundee@gmail.com>
2014-12-02 00:03:52 +0000

    added format-duration helper and page for each individual album

---
+++ tests/unit/helpers/format-duration-test.js
@@ -0,0 +1,11 @@
+import {
+  formatDuration
+} from 'bumbox/helpers/format-duration';
+
+module('FormatDurationHelper');
+
+// Replace this with your real tests.
+test('it works', function() {
+  var result = formatDuration(42);
+  ok(result);
+});
