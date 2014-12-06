65974ff7ee3446483f1fe01a276d876aa22b99b4
codepreneur <contramundee@gmail.com>
2014-12-06 09:15:58 +0200

    updated album model and song count

--- app/models/album.js
+++ app/models/album.js
@@ -1,11 +1,18 @@
 import DS from 'ember-data';
+import Ember from 'ember';
+
 var attr = DS.attr,
-    hasMany = DS.hasMany;
+    hasMany = DS.hasMany,
+    computed = Ember.computed;
 
 export default DS.Model.extend({
   artwork: attr('string'),
   name: attr('string'),
   artist: attr('string'),
   isExplicit: attr('boolean'),
-  songs: hasMany('song')
+  songs: hasMany('song'),
+
+  songDurations: computed.mapBy('songs', 'duration'),
+  totalDuration: computed.sum('songDurations'),
+  songCount: computed.alias('songs.length')
 });
