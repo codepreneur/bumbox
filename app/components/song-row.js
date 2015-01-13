be88071d9b54a3aef9b9fed57a618e170e78cd74
codepreneur <contramundee@gmail.com>
2015-01-13 00:19:51 +0000

    updated song-row component

--- app/components/song-row.js
+++ app/components/song-row.js
@@ -2,15 +2,27 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'tr',
-  isPlaying: false,
+  classNameBindings: ['isCurrentSong'],
+
+  song: null,
+
+  player: inject.service(),
 
   actions: {
     play: function() {
-      this.set('isPlaying', true);
+      this.get('player').play(this.get('song'));
     },
 
     pause: function() {
-      this.set('isPlaying', false);
+      this.get('player').pause();
     }
-  }
+  },
+
+  isCurrentSong: function() {
+    return this.get('player.song') === this.get('song');
+  }.property('player.song', 'song'),
+
+  isPlaying: function() {
+    return this.get('isCurrentSong') && this.get('player.isPlaying');
+  }.property('isCurrentSong', 'player.isPlaying')
 });
