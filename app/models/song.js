f0de30e514c3736357450d59f754d984833e720f
codepreneur <contramundee@gmail.com>
2015-01-15 23:46:22 +0000

    updated song model

--- app/models/song.js
+++ app/models/song.js
@@ -1,12 +1,18 @@
 import DS from 'ember-data';
+import Ember from 'ember';
+
 var attr = DS.attr,
-    belongsTo = DS.belongsTo;
+    belongsTo = DS.belongsTo,
+    computed = Ember.computed;
 
 export default DS.Model.extend({
   track: attr('number'),
   name: attr('string'),
   duration: attr('number'),
   url: attr('string'),
-  album: belongsTo('album')
+  album: belongsTo('album'),
+
+  artist: computed.alias('album.artist'),
+  artwork: computed.alias('album.artwork')
 });
 
